2f2cc31c214dce7f8c3dc7db88144057dea7dde4
Bruno Drugowick <bruno.drugowick@gmail.com>
2021-04-11 18:22:39 -0300

    Changing a few things to prepare for the next feature (notification to the left)

--- src/extension.js
+++ src/extension.js
@@ -5,11 +5,14 @@ const Main = imports.ui.main;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
-
 // Like `init()` below, code *here* in the top-level of your script is executed
 // when your extension is loaded. You MUST NOT make any changes to GNOME Shell
 // here and typically you should do nothing but assign variables.
 const PADDING = 20;
+const monitorWidth = Main.layoutManager.monitors[0].width;
+const monitorHeight = Main.layoutManager.monitors[0].height;
+const messageListWidth = Main.panel.statusArea.dateMenu._messageList.actor.width;
+const messageListHeight = Main.panel.statusArea.dateMenu._messageList.actor.height;
 
 // This function is called once when your extension is loaded, not enabled. This
 // is a good time to setup translations or anything else you only do once.
@@ -18,6 +21,16 @@ const PADDING = 20;
 // MainLoop sources here.
 function init() {
     log(`initializing ${Me.metadata.name} version ${Me.metadata.version}`);
+}
+
+function left()
+{
+    return - monitorWidth + messageListWidth + PADDING;
+}
+
+function right()
+{
+    return monitorWidth - messageListWidth - PADDING;
 }
 
 // This function could be called after your extension is enabled, which could
@@ -31,30 +44,14 @@ function enable() {
     Main.messageTray._bannerBin.x = right();
 }
 
-function left()
-{
-    const monitorWidth = Main.layoutManager.monitors[0].width;
-    const messageListWidth = Main.panel.statusArea.dateMenu._messageList.actor.width;
-
-    Main.messageTray._bannerBin.x = - monitorWidth + messageListWidth;
-}
-
-function right()
-{
-    const monitorWidth = Main.layoutManager.monitors[0].width;
-    const messageListWidth = Main.panel.statusArea.dateMenu._messageList.actor.width;
-
-    Main.messageTray._bannerBin.x = monitorWidth - messageListWidth - PADDING;
-}
-
 // This function could be called after your extension is uninstalled, disabled
 // in GNOME Tweaks, when you log out or when the screen locks.
 //
 // Anything you created, modifed or setup in enable() MUST be undone here. Not
-
 // doing so is the most common reason extensions are rejected during review!
 function disable() {
     log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`);
     Main.messageTray._bannerBin.x = 0;
+    Main.messageTray._bannerBin.y = 0;
 }
 
