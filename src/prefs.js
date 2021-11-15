cd4b1b52cf471249a535a87fedb4b53643a893b7
Bruno Drugowick <bruno.drugowick@gmail.com>
2021-11-14 21:43:44 -0300

    WIP: creating settings schema

--- src/prefs.js
+++ src/prefs.js
@@ -18,10 +18,55 @@ function init() {
 function buildPrefsWidget() {
     // This could be any GtkWidget subclass, although usually you would choose
     // something like a GtkGrid, GtkBox or GtkNotebook
-    let bannerPositionLabel = new Gtk.Label({
-        label: `${Me.metadata.name}`,
-        visible: true
+    let widget = new PrefsWidget();
+    widget.show_all();
+    return widget;
+}
+
+const PrefsWidget = GObject.registerClass(
+class PrefsWidget extends Gtk.Box {
+
+  _init (params) {
+
+    // Initialize widget
+    super._init(params);
+    this.margin = 20;
+    this.set_spacing(15);
+    this.set_orientation(Gtk.Orientation.VERTICAL);
+    this.connect('destroy', Gtk.main_quit);
+
+    // Label
+    let myLabel = new Gtk.Label({
+      label : "Notification banner position"    
     });
 
-    return new bannerPositionLabel;
-}
+    // Combobox
+    let model = new Gtk.ListStore();
+    model.set_column_types([GObject.TYPE_STRING, GObject.TYPE_STRING]);
+    let cbox = new Gtk.ComboBox({model: model});
+    let renderer = new Gtk.CellRendererText();
+    cbox.pack_start(renderer, true);
+    cbox.add_attribute(renderer, 'text', 1);
+    model.set(model.append(), [0, 1], ['left', 'Left']);
+    model.set(model.append(), [0, 1], ['right', 'Right']);
+    //cbox.set_active(0); // set value
+
+    cbox.connect('changed', function(entry) {
+        let [success, iter] = cbox.get_active_iter();
+        if (!success)
+            return;
+        let myValue = model.get_value(iter, 0); // get value
+    });
+    
+    // Horizontal box to house everything
+    let hBox = new Gtk.Box();
+    hBox.set_orientation(Gtk.Orientation.HORIZONTAL);
+    hBox.pack_start(myLabel, false, false, 0);
+    hBox.pack_end(cbox, false, false, 0);
+
+    // Add box to widget
+    this.add(hBox);
+  }
+
+});
+
