460a4efda019c3dcac261322672ee9e143fc98aa
Bruno Drugowick <brunodrugowick@users.noreply.github.com>
2023-03-22 21:56:58 -0300

    Add script to auto update according to Gnome releases

---
+++ scripts/auto-update-from-gnome-release.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+add_gnome_support() {
+	echo "Adding support to Gnome version" $1
+	cd $ROOT_PROJECT_DIR
+	cat src/metadata.json \
+		| jq --arg version "$1" '."shell-version" += [$version]' \
+		| jq --arg version "$1" '.version = $version' \
+		> src/metadata.json.tmp # it fails if I write directly to the same file
+	mv src/metadata.json.tmp src/metadata.json
+}
+
+ROOT_PROJECT_DIR=$(pwd)
+TEMP_DIR=$(mktemp -d)
+
+cp CURRENT_SUPPORTED_GNOME_VERSION $TEMP_DIR
+cd $TEMP_DIR
+
+git clone --depth=1 https://gitlab.gnome.org/GNOME/gnome-software.git
+cd gnome-software
+git fetch --tags
+
+CURRENT_SUPPORTED_GNOME_VERSION=$(cat ../CURRENT_SUPPORTED_GNOME_VERSION)
+echo "Current supported Gnome version is" $CURRENT_SUPPORTED_GNOME_VERSION
+
+for GNOME_TAG in $(git tag -l); do
+	echo "Analyzing TAG" $GNOME_TAG
+	IS_NEW_VERSION=$(bc -ql <<< "$GNOME_TAG > $CURRENT_SUPPORTED_GNOME_VERSION")
+	if [[ $IS_NEW_VERSION -eq 1 ]]; then
+		echo "Looks like there's a new valid version -->" $GNOME_TAG
+		add_gnome_support $GNOME_TAG
+		break
+	else
+		echo "Ignored tag -->" $GNOME_TAG
+	fi
+done
+
