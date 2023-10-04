30268a69a219c24047ba41914ee907ef7ed04b99
Bruno Drugowick <brunodrugowick@users.noreply.github.com>
2023-10-03 21:17:58 -0300

    Add support to Gnome 45 (based on https://gjs.guide/extensions/upgrading/gnome-shell-45.html)

--- src/extension.js
+++ src/extension.js
@@ -1,58 +1,59 @@
-'use strict';
+import Clutter from 'gi://Clutter';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as ExtensionUtils from 'resource:///org/gnome/shell/misc/extensionUtils.js';
 
-// This is a handy import we'll use to grab our extension's object
-const Main = imports.ui.main;
-const { Clutter } = imports.gi;
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
+export default class NotificationPosition {
 
-// Like `init()` below, code *here* in the top-level of your script is executed
-// when your extension is loaded. You MUST NOT make any changes to GNOME Shell
-// here and typically you should do nothing but assign variables.
-const _originalBannerAlignment = Main.messageTray.bannerAlignment;
+    const Me = ExtensionUtils.getCurrentExtension();
 
-// This function is called once when your extension is loaded, not enabled. This
-// is a good time to setup translations or anything else you only do once.
-//
-// You MUST NOT make any changes to GNOME Shell, connect any signals or add any
-// MainLoop sources here.
-function init() {
-    log(`initializing ${Me.metadata.name} version ${Me.metadata.version}`);
-}
+    // Like `init()` below, code *here* in the top-level of your script is executed
+    // when your extension is loaded. You MUST NOT make any changes to GNOME Shell
+    // here and typically you should do nothing but assign variables.
+    const _originalBannerAlignment = Main.messageTray.bannerAlignment;
 
-function left() {
-    Main.messageTray.bannerAlignment = Clutter.ActorAlign.START;
-}
+    // This function is called once when your extension is loaded, not enabled. This
+    // is a good time to setup translations or anything else you only do once.
+    //
+    // You MUST NOT make any changes to GNOME Shell, connect any signals or add any
+    // MainLoop sources here.
+    init() {
+        log(`initializing ${Me.metadata.name} version ${Me.metadata.version}`);
+    }
 
-function right() {
-    Main.messageTray.bannerAlignment = Clutter.ActorAlign.END;
-}
+    left() {
+        Main.messageTray.bannerAlignment = Clutter.ActorAlign.START;
+    }
 
-function middle() {
-    Main.messageTray.bannerAlignment = Clutter.ActorAlign.CENTER;
-}
+    right() {
+        Main.messageTray.bannerAlignment = Clutter.ActorAlign.END;
+    }
 
-function _original() {
-    Main.messageTray.bannerAlignment = _originalBannerAlignment;
-}
+    middle() {
+        Main.messageTray.bannerAlignment = Clutter.ActorAlign.CENTER;
+    }
 
-// This function could be called after your extension is enabled, which could
-// be done from GNOME Tweaks, when you log in or when the screen is unlocked.
-//
-// This is when you setup any UI for your extension, change existing widgets,
-// connect signals or modify GNOME Shell's behaviour.
-function enable() {
-    log(`enabling ${Me.metadata.name} version ${Me.metadata.version}`);
-    right();
-}
+    _original() {
+        Main.messageTray.bannerAlignment = _originalBannerAlignment;
+    }
 
-// This function could be called after your extension is uninstalled, disabled
-// in GNOME Tweaks, when you log out or when the screen locks.
-//
-// Anything you created, modifed or setup in enable() MUST be undone here. Not
-// doing so is the most common reason extensions are rejected during review!
-function disable() {
-    log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`);
-    _original();
+    // This function could be called after your extension is enabled, which could
+    // be done from GNOME Tweaks, when you log in or when the screen is unlocked.
+    //
+    // This is when you setup any UI for your extension, change existing widgets,
+    // connect signals or modify GNOME Shell's behaviour.
+    enable() {
+        log(`enabling ${Me.metadata.name} version ${Me.metadata.version}`);
+        right();
+    }
+
+    // This function could be called after your extension is uninstalled, disabled
+    // in GNOME Tweaks, when you log out or when the screen locks.
+    //
+    // Anything you created, modified or setup in enable() MUST be undone here. Not
+    // doing so is the most common reason extensions are rejected during review!
+    disable() {
+        log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`);
+        _original();
+    }
 }
 
